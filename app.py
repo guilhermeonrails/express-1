eb5411ec1676ec1811988cdc9a36cdbef40e5f4a
guilhermelima <guilhermelimaruby@gmail.com>
2023-10-18 17:23:01 -0300

    projeto curso 2

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
+from models.restaurante import Restaurante
 
-restaurantes = [
-    {'nome': 'Praça', 'categoria': 'Gourmet', 'aberto': None},
-    {'nome': 'Cantina da Nonna', 'categoria': 'Italiana', 'aberto': True},
-    {'nome': 'Sushi', 'categoria': 'Japonesa', 'aberto': False},
-    {'nome': 'Pizza Suprema', 'categoria': 'Pizza', 'aberto': False}
-]
+restaurante_praca = Restaurante(nome='Praça', categoria='Gourmet')
+restaurante_nonna = Restaurante(nome='Nonna', categoria='Italiana')
+restaurante_sushi = Restaurante(nome='Sushi', categoria='Japonesa')
+restaurante_pizza = Restaurante(nome='Pizza Supreme', categoria='Pizza')
+
+restaurantes = [restaurante_praca, restaurante_nonna, restaurante_sushi, restaurante_pizza]
 
 cor_vermelho = "\033[41m"
 cor_azul = "\033[34m"
@@ -13,20 +14,13 @@ cor_verde = "\033[32m"
 reset_cor = "\033[0m"
 
 def exibir_nome_do_programa():
-    print('''
-░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
-██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
-╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
-░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
-██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
-''')
+    print('''Sabor express''')
 
 def finalizar_app():
-    os.system('clear')
     exibir_subtitulos('Finalizando o app')
 
 def exibir_subtitulos(texto):
+    os.system('clear')
     linha = '*' * (len(texto) + 4)
     texto_personalizado = f' {texto} ' 
     print(linha)
@@ -40,55 +34,52 @@ def exibir_opcoes():
     print('➤ 3. Ativar restaurantes')
     print('➤ 4. Sair\n')
 
-def calcular_espacamento(chave):
-    return max((len(restaurante[chave]) for restaurante in restaurantes)) + 2
+def calcular_espacamento():
+    return max((len(restaurante.nome) for restaurante in restaurantes)) + 2, max((len(restaurante.categoria) for restaurante in restaurantes)) + 2
 
 def opcao_invalida():
     print(f'{cor_vermelho}{"❌ Opção inválida"}{reset_cor}')
     input('\nPressione enter para voltar ao menu e escolha uma opção válida! ')
 
 def cadastrar_restaurante():
-    os.system('clear')
     exibir_subtitulos('Adicionar restaurante')
     nome_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     categoria = input(f'Qual a categoria do restaurante {nome_restaurante}: ')
-    dados_novo_restaurante = {'nome': nome_restaurante, 'categoria': categoria, 'aberto': False}
+    dados_novo_restaurante = Restaurante(nome_restaurante, categoria)
     restaurantes.append(dados_novo_restaurante)
 
 def listar_restaurantes():
-    os.system('clear')
     exibir_subtitulos('Listando restaurantes')
-    espacamento_nome = calcular_espacamento('nome')
-    espacamento_categoria = calcular_espacamento('categoria')
+    espacamento_nome, espacamento_categoria = calcular_espacamento()
     
     print(f'{cor_verde}{"Restaurantes".ljust(espacamento_nome)} | {"Categoria".ljust(espacamento_categoria)} | Aberto{reset_cor}')
 
     for restaurante in restaurantes:
-        nome = restaurante['nome'].ljust(espacamento_nome)
-        categoria = restaurante['categoria'].ljust(espacamento_categoria)
-        aberto = '✔️' if restaurante['aberto'] else '☐'
+        nome = restaurante.nome.ljust(espacamento_nome)
+        categoria = restaurante.categoria.ljust(espacamento_categoria)
+        aberto = '✔️' if restaurante.aberto else '☐'
         print(f"{nome} | {categoria.upper()} | ⇒ {aberto}")
 
     input('\nPressione enter para voltar ')
 
 def ativar_restaurante():
-    os.system('clear')
     exibir_subtitulos('Ativação de restaurante')
     nome_restaurante_para_ativar = input('Digite o nome do restaurante que deseja ativar: ')
-    restaurante_encontrado = False  # Variável para rastrear se o restaurante foi encontrado
+    restaurante_encontrado = False
     
     for restaurante in restaurantes:
-        if nome_restaurante_para_ativar == restaurante['nome']:
-            restaurante['aberto'] = not restaurante['aberto']
-            mensagem = f'O {nome_restaurante_para_ativar} foi ativado com sucesso!' if restaurante['aberto'] else f'O {nome_restaurante_para_ativar} foi desativado com sucesso!'
+        if nome_restaurante_para_ativar == restaurante.nome:
+            restaurante.aberto = restaurante.alternar_estado()
+            print(restaurante.aberto)
+            mensagem = f'O {restaurante.nome} foi ativado com sucesso!' if restaurante.aberto else f'O {restaurante.nome} foi desativado com sucesso!'
             print(mensagem)
             restaurante_encontrado = True
-            break  # Saia do loop assim que encontrar o restaurante
+            break
             
     if not restaurante_encontrado:
         print(f'{cor_vermelho}❌ Restaurante não encontrado{reset_cor}')
     
-    input('\nPressione enter para voltar ao menu e escolha uma opção válida! ')
+    input('\nPressione enter para voltar ao menu e escolher uma opção válida! ')
 
 while True:
     os.system('clear')
