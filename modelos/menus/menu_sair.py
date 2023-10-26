edab88242f9d97f20408dff429373b2ba8fc5bc6
guilhermelima <guilhermelimaruby@gmail.com>
2023-10-26 09:46:47 -0300

    inicio_curso_2

--- modelos/menus/menu_sair.py
+++
@@ -1,7 +0,0 @@
-from modelos.menus.menu import Menu
-
-class MenuSair(Menu):
-    
-    def __init__(self):
-        super().exibir_subtitulos('Finalizando o app')
-         # Usando 'super()' para chamar o método da classe pai
